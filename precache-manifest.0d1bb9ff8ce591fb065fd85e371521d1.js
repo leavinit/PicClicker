02c17e79d2ee7cb557c660a55791e527723eaf53
Adam S <leavinit@gmail.com>
2019-06-22 00:16:27 -0500

    Updates

---
+++ precache-manifest.0d1bb9ff8ce591fb065fd85e371521d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3696befe275869b9a8c0e9945981b1",
+    "url": "/PicClicker/index.html"
+  },
+  {
+    "revision": "44da1d9fb58a282a53ab",
+    "url": "/PicClicker/static/css/main.eb8b8609.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/PicClicker/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "44da1d9fb58a282a53ab",
+    "url": "/PicClicker/static/js/main.cd190e2c.chunk.js"
+  },
+  {
+    "revision": "7a2f49384ec571a66960",
+    "url": "/PicClicker/static/js/runtime~main.9be79aef.js"
+  }
+]);
